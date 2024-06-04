2fe6d7dbc1b54bc82158ab43b17a2380114de6e5
Yohan Pruche <Yohan.Pruche@orange.fr>
2024-06-04 16:34:13 +0200

    ajout Footer

--- my-react-ts-app/src/components/Footer/index.tsx
+++ my-react-ts-app/src/components/Footer/index.tsx
@@ -1,3 +1,38 @@
+import styled from "styled-components";
+import gitHubLogo from "../../assets/images/github-mark-white.png";
+
 export default function Footer() {
-	return <div>le footer</div>;
+	return (
+		<FooterStyle>
+			<div className="footer__logo">
+				<a href="https://github.com/Phanttoms" target="_blank">
+					<img src={gitHubLogo} alt="GitHub Logo" />
+				</a>
+			</div>
+			<div className="footer__text">
+				2024 Yohan PRUCHE, Tous droits rèservés.
+			</div>
+		</FooterStyle>
+	);
 }
+
+const FooterStyle = styled.footer`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	gap: 1rem;
+	background-color: black;
+	height: 10rem;
+	width: 100%;
+	.footer__logo {
+		a {
+			img {
+				width: 2rem;
+			}
+		}
+	}
+	.footer__text {
+		color: white;
+	}
+`;
