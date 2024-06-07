02699ae1b91e1a6d3ab4f6cdc0e215137c2b75a2
Yohan Pruche <Yohan.Pruche@orange.fr>
2024-06-07 16:05:00 +0200

    delete du scss

--- my-react-ts-app/src/App.tsx
+++ my-react-ts-app/src/App.tsx
@@ -1,4 +1,3 @@
-// import styled from "styled-components";
 import RoutesPath from "./components/Router";
 import GlobalStyle from "./assets/style/GlobalStyle";
 
@@ -12,37 +11,3 @@ function App() {
 }
 
 export default App;
-
-// const MainStyle = styled.div`
-// 	body {
-// 		margin: 0;
-// 		display: flex;
-// 		flex-direction: column;
-// 	}
-
-// 	ul,
-// 	li,
-// 	h1,
-// 	h2,
-// 	hr,
-// 	p {
-// 		margin: 0;
-// 		padding: 0;
-// 	}
-
-// 	a {
-// 		text-decoration: none;
-// 		color: black;
-// 		cursor: pointer;
-// 	}
-
-// 	h1,
-// 	h2,
-// 	li {
-// 		font-family: "roboto", sans-serif;
-// 	}
-
-// 	p {
-// 		font-family: "Open Sans", sans-serif;
-// 	}
-// `;
