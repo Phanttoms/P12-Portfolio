4ffeb76f7bdaed64356533225f575b6261df9931
Yohan Pruche <Yohan.Pruche@orange.fr>
2024-06-17 12:47:43 +0200

    ajout burger menu

--- my-react-ts-app/src/components/Header/index.tsx
+++ my-react-ts-app/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import styled from "styled-components";
 import logo from "../../assets/images/logos/logoSite.png";
 import { theme } from "../../assets/style/Theme";
@@ -17,13 +18,27 @@ const scrollToSection = (id: string) => {
 };
 
 export default function Header() {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen);
+	};
+
 	return (
 		<HeaderStyle>
 			<Link to="/" className="test">
 				<img className="header__logo" src={logo} alt="logo du site" />
 			</Link>
 			<h1 className="header__title">Pruche Yohan</h1>
-			<nav className="header__navbar">
+			<div
+				className={`burger-menu ${isMenuOpen ? "open" : ""}`}
+				onClick={toggleMenu}
+			>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<nav className={`header__navbar ${isMenuOpen ? "open" : ""}`}>
 				<ul>
 					<li onClick={() => scrollToSection("section1")}>
 						<div>
@@ -55,17 +70,17 @@ const HeaderStyle = styled.header`
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
-	flex-direction: row;
 	width: 100%;
-	height: 6rem;
-	overflow: hidden;
+	height: 7rem;
 	background-color: ${theme.colors.black};
 	position: fixed;
 	top: 0;
 	left: 0;
 	border: 0.1rem solid ${theme.colors.green};
-	box-sizing: border-box;
 	z-index: 1000;
+	padding: 0 1rem;
+	box-sizing: border-box;
+
 	.header__title {
 		position: absolute;
 		width: 1px;
@@ -76,44 +91,52 @@ const HeaderStyle = styled.header`
 		clip: rect(0, 0, 0, 0);
 		border: 0;
 	}
+
 	.header__logo {
-		width: 7rem;
-		height: 6rem;
+		width: 8rem;
+		height: 6.5rem;
 	}
+
 	.header__navbar {
+		display: flex;
+		padding-right: 2rem;
+		box-sizing: border-box;
 		ul {
 			list-style: none;
 			display: flex;
 			flex-direction: row;
-			width: 100%;
-			justify-content: space-between;
-			padding-right: 2rem;
-			box-sizing: border-box;
+			padding: 0;
+			margin: 0;
 			gap: 1rem;
+			box-sizing: border-box;
+
 			li {
-				width: 7.2rem;
-				height: 3.2rem;
+				width: 8.2rem;
+				height: 4.2rem;
 				background-color: ${theme.colors.green};
 				display: flex;
 				justify-content: center;
 				align-items: center;
 				clip-path: polygon(30% 0, 100% 0, 100% 70%, 70% 100%, 0 100%, 0 30%);
 				transition: 0.3s ease-in-out;
+				box-sizing: border-box;
+
 				&:hover {
-					/* transform: scale(1.1); */
 					clip-path: polygon(70% 0, 100% 30%, 100% 100%, 30% 100%, 0 70%, 0 0);
 				}
+
 				div {
-					width: 7rem;
-					height: 3rem;
+					width: 8rem;
+					height: 4rem;
 					background: linear-gradient(145deg, #444, #222);
 					display: flex;
 					justify-content: center;
 					align-items: center;
 					clip-path: polygon(30% 0, 100% 0, 100% 70%, 70% 100%, 0 100%, 0 30%);
 					transition: 0.3s ease-in-out;
+					box-sizing: border-box;
+
 					&:hover {
-						/* transform: scale(1.1); */
 						clip-path: polygon(
 							70% 0,
 							100% 30%,
@@ -123,8 +146,68 @@ const HeaderStyle = styled.header`
 							0 0
 						);
 					}
+
+					a {
+						font-size: 1.2rem;
+						text-decoration: none;
+					}
 				}
 			}
+		}
+	}
+
+	.burger-menu {
+		display: none;
+		flex-direction: column;
+		cursor: pointer;
+		margin-right: 1rem;
+
+		span {
+			background-color: ${theme.colors.green};
+			height: 0.4rem;
+			width: 2.5rem;
+			margin: 0.4rem;
+			transition: 0.3s ease-in-out;
+		}
+	}
+
+	/* Media Queries for Mobile */
+	@media (max-width: 768px) {
+		.header__navbar ul {
+			display: none;
+			flex-direction: column;
+			position: absolute;
+			top: 6.8rem;
+			right: 0;
+			background-color: ${theme.colors.black};
+			margin: 0;
+			border: 0.1rem solid ${theme.colors.green};
+			border-top: hidden;
+			box-sizing: border-box;
+			align-items: flex-end;
+			padding: 1rem;
+		}
+
+		.burger-menu {
+			display: flex;
+			position: absolute;
+			right: 0;
+		}
+
+		.burger-menu.open span:nth-child(1) {
+			transform: rotate(45deg) translate(0.5rem, 0.5rem);
+		}
+
+		.burger-menu.open span:nth-child(2) {
+			opacity: 0;
+		}
+
+		.burger-menu.open span:nth-child(3) {
+			transform: rotate(-45deg) translate(0.5rem, -0.5rem);
+		}
+
+		.header__navbar.open ul {
+			display: flex;
 		}
 	}
 `;
