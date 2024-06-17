041c845bf8bc8d7633fb32c324b753c0e7317076
Yohan Pruche <Yohan.Pruche@orange.fr>
2024-06-18 01:30:57 +0200

    fix tooltip

--- my-react-ts-app/src/components/Presentation/index.tsx
+++ my-react-ts-app/src/components/Presentation/index.tsx
@@ -8,11 +8,27 @@ export default function Presentation() {
 			<hr />
 			<div className="presentation__content">
 				<p className="presentation__content--text">
-					ceci est l'endroit où il va y avoir du text pour expliquer <br /> des
-					trucs blablabla blabla bla bla blablablabla
-					<br /> bla blablba bla blabla bla bla blablablabla bla blablba bla
-					blabla bla bla blablablabla <br /> bla blablba bla blabla bla bla
-					blablablabla bla blablba bla
+					Je suis un jeune développeur web passionné par le web et l'
+					<strong>informatique</strong>.<br />
+					Ma fascination pour le monde numérique m'a poussé à explorer diverses
+					technologies et à développer des compétences solides dans la création
+					de sites et d'applications web.
+					<br />
+					<br />
+					Toujours <strong>curieux</strong> et avide d'
+					<strong>apprendre</strong>, je suis déterminé à{" "}
+					<strong>innover</strong> et à apporter des solutions{" "}
+					<strong>efficaces</strong> et <strong>créatives</strong> dans chaque
+					projet que j'entreprends.
+					<br />
+					<br />À travers mes expériences, j'ai acquis une solide expertise dans
+					des technologies telles que <strong>HTML</strong>,{" "}
+					<strong>CSS</strong>, <strong>JavaScript</strong> ainsi que des
+					frameworks comme <strong>React</strong>.<br />
+					<br />
+					Je suis constamment en train d'explorer de{" "}
+					<strong>nouvelles technologies</strong> pour rester à la{" "}
+					<strong>pointe</strong> de mon domaine.
 				</p>
 			</div>
 		</PresentationStyle>
--- my-react-ts-app/src/components/Skill/index.tsx
+++ my-react-ts-app/src/components/Skill/index.tsx
@@ -1,117 +1,3 @@
-// import styled from "styled-components";
-// import front1 from "../../assets/images/logos/logoHTML.png";
-// import front2 from "../../assets/images/logos/logoCSS.png";
-// import front3 from "../../assets/images/logos/logoJS.png";
-// import front4 from "../../assets/images/logos/logoTypeScript.svg";
-// import front5 from "../../assets/images/logos/logoReact.svg";
-// import front6 from "../../assets/images/logos/logoSass.svg";
-
-// import back1 from "../../assets/images/logos/logoNode.png";
-// import back2 from "../../assets/images/logos/logoExpress.png";
-// import back3 from "../../assets/images/logos/logoMongoDB.png";
-
-// import tool1 from "../../assets/images/logos/logoGit.png";
-// import tool2 from "../../assets/images/logos/logoGitHub.png";
-// import tool3 from "../../assets/images/logos/logoVercel.svg";
-// import tool4 from "../../assets/images/logos/logoRedux.svg";
-// import tool5 from "../../assets/images/logos/logoStyled.svg";
-// import tool6 from "../../assets/images/logos/logoVite.svg";
-// import { theme } from "../../assets/style/Theme";
-
-// export default function Skill() {
-// 	return (
-// 		<SkillStyle id="section2">
-// 			<h2>Compétences</h2>
-// 			<hr />
-// 			<div className="skill__container">
-// 				<div className="skill__container__content">
-// 					<h3 className="skill__container__content--title">Front-End</h3>
-// 					<div>
-// 						<img src={front1} className="skill__container__content--img" />
-// 						<img src={front2} className="skill__container__content--img" />
-// 						<img src={front3} className="skill__container__content--img" />
-// 						<img src={front4} className="skill__container__content--img" />
-// 						<img src={front5} className="skill__container__content--img" />
-// 						<img src={front6} className="skill__container__content--img" />
-// 					</div>
-// 				</div>
-// 				<div className="skill__container__content">
-// 					<h3 className="skill__container__content--title">Back-End</h3>
-// 					<div>
-// 						<img src={back1} className="skill__container__content--img" />
-// 						<img src={back2} className="skill__container__content--img" />
-// 						<img src={back3} className="skill__container__content--img" />
-// 					</div>
-// 				</div>
-// 				<div className="skill__container__content">
-// 					<h3 className="skill__container__content--title">
-// 						Outils et services
-// 					</h3>
-// 					<div>
-// 						<img src={tool1} className="skill__container__content--img" />
-// 						<img src={tool2} className="skill__container__content--img" />
-// 						<img src={tool3} className="skill__container__content--img" />
-// 						<img src={tool4} className="skill__container__content--img" />
-// 						<img src={tool5} className="skill__container__content--img" />
-// 						<img src={tool6} className="skill__container__content--img" />
-// 					</div>
-// 				</div>
-// 			</div>
-// 		</SkillStyle>
-// 	);
-// }
-
-// const SkillStyle = styled.section`
-// 	display: flex;
-// 	flex-direction: column;
-// 	align-items: center;
-// 	padding: 2rem 0;
-// 	.skill__container {
-// 		display: flex;
-// 		flex-wrap: wrap;
-// 		justify-content: center;
-// 		flex-direction: column;
-// 		padding: 2rem;
-// 		margin: 0 3rem;
-// 		gap: 2rem;
-// 		background-color: ${theme.colors.black};
-// 		border: 0.1rem solid ${theme.colors.green};
-// 		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-// 		border-radius: 2%;
-// 		&__content {
-// 			display: flex;
-// 			flex-direction: column;
-// 			align-items: center;
-// 			width: 100%;
-// 			border: 0.1rem solid ${theme.colors.green};
-// 			background-color: white;
-// 			box-shadow: 2px 4px 5px rgba(12, 207, 12, 0.616);
-// 			gap: 1rem;
-// 			height: 100%;
-
-// 			&--title {
-// 				background: linear-gradient(145deg, #444, #222);
-// 				box-shadow: inset 1px 1px 2px rgba(255, 255, 255, 0.1),
-// 					inset -1px -1px 2px rgba(0, 0, 0, 0.7), 1px 1px 3px rgba(0, 0, 0, 0.5);
-// 				border: 0.1rem solid ${theme.colors.green};
-// 				width: 100%;
-// 				text-align: center;
-// 				padding: 0.7rem 0;
-// 			}
-// 			div {
-// 				padding: 0.5rem;
-// 				display: flex;
-// 				flex-wrap: wrap;
-// 				gap: 1rem;
-// 			}
-// 			&--img {
-// 				width: 8rem;
-// 			}
-// 		}
-// 	}
-// `;
-
-import React, { useState } from "react";
 import styled from "styled-components";
 import front1 from "../../assets/images/logos/logoHTML.png";
 import front2 from "../../assets/images/logos/logoCSS.png";
@@ -131,26 +17,17 @@ import tool4 from "../../assets/images/logos/logoRedux.svg";
 import tool5 from "../../assets/images/logos/logoStyled.svg";
 import tool6 from "../../assets/images/logos/logoVite.svg";
 import { theme } from "../../assets/style/Theme";
+import { useState } from "react";
 
 const Skill = () => {
-	const [hoveredSkill, setHoveredSkill] = useState<string | null>(null);
-	const [tooltipPosition, setTooltipPosition] = useState<{
-		top: number;
-		left: number;
-	}>({ top: 0, left: 0 });
+	const [hoveredSkill, setHoveredSkill] = useState(null);
 
-	const handleMouseMove = (
-		e: React.MouseEvent<HTMLImageElement>,
-		skill: string
-	) => {
-		const rect = e.currentTarget.getBoundingClientRect();
-		setTooltipPosition({
-			top: rect.bottom + window.scrollY - 350,
-			left: rect.left + window.scrollX + rect.width / 2,
-		});
-		setHoveredSkill(skill);
+	// Fonction pour gérer le survol d'une image
+	const handleMouseEnter = (alt: any) => {
+		setHoveredSkill(alt);
 	};
 
+	// Fonction pour gérer la fin du survol
 	const handleMouseLeave = () => {
 		setHoveredSkill(null);
 	};
@@ -188,14 +65,21 @@ const Skill = () => {
 					<h3 className="skill__container__content--title">Front-End</h3>
 					<div>
 						{frontSkills.map((skill) => (
-							<img
+							<div
 								key={skill.alt}
-								src={skill.src}
-								alt={skill.alt}
-								className="skill__container__content--img"
-								onMouseMove={(e) => handleMouseMove(e, skill.description)}
+								className="skill__container__content--imgContainer"
+								onMouseEnter={() => handleMouseEnter(skill.alt)}
 								onMouseLeave={handleMouseLeave}
-							/>
+							>
+								<img
+									src={skill.src}
+									alt={skill.alt}
+									className="skill__container__content--img"
+								/>
+								{hoveredSkill === skill.alt && (
+									<div className="skill__tooltip">{skill.description}</div>
+								)}
+							</div>
 						))}
 					</div>
 				</div>
@@ -203,14 +87,21 @@ const Skill = () => {
 					<h3 className="skill__container__content--title">Back-End</h3>
 					<div>
 						{backSkills.map((skill) => (
-							<img
+							<div
 								key={skill.alt}
-								src={skill.src}
-								alt={skill.alt}
-								className="skill__container__content--img"
-								onMouseMove={(e) => handleMouseMove(e, skill.description)}
+								className="skill__container__content--imgContainer"
+								onMouseEnter={() => handleMouseEnter(skill.alt)}
 								onMouseLeave={handleMouseLeave}
-							/>
+							>
+								<img
+									src={skill.src}
+									alt={skill.alt}
+									className="skill__container__content--img"
+								/>
+								{hoveredSkill === skill.alt && (
+									<div className="skill__tooltip">{skill.description}</div>
+								)}
+							</div>
 						))}
 					</div>
 				</div>
@@ -220,25 +111,24 @@ const Skill = () => {
 					</h3>
 					<div>
 						{tools.map((tool) => (
-							<img
+							<div
 								key={tool.alt}
-								src={tool.src}
-								alt={tool.alt}
-								className="skill__container__content--img"
-								onMouseMove={(e) => handleMouseMove(e, tool.description)}
+								className="skill__container__content--imgContainer"
+								onMouseEnter={() => handleMouseEnter(tool.alt)}
 								onMouseLeave={handleMouseLeave}
-							/>
+							>
+								<img
+									src={tool.src}
+									alt={tool.alt}
+									className="skill__container__content--img"
+								/>
+								{hoveredSkill === tool.alt && (
+									<div className="skill__tooltip">{tool.description}</div>
+								)}
+							</div>
 						))}
 					</div>
 				</div>
-				{hoveredSkill && (
-					<div
-						className="tooltip"
-						style={{ top: tooltipPosition.top, left: tooltipPosition.left }}
-					>
-						{hoveredSkill}
-					</div>
-				)}
 			</div>
 		</SkillStyle>
 	);
@@ -297,18 +187,24 @@ const SkillStyle = styled.section`
 				cursor: pointer;
 				position: relative;
 			}
+			&--imgContainer {
+				display: flex;
+				flex-direction: column;
+				position: relative;
+			}
 		}
 	}
-
-	.tooltip {
+	.skill__tooltip {
 		position: absolute;
 		background-color: ${theme.colors.green};
 		color: white;
+		box-shadow: 0 0.2rem 0.5rem rgba(0, 0, 0, 0.2);
 		padding: 0.5rem;
 		border-radius: 0.5rem;
-		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-		transform: translate(-50%, 0); /* Center horizontally */
-		white-space: nowrap;
+		z-index: 10;
+		top: 90%;
+		left: 50%;
+		transform: translate(-50%, 0%);
 	}
 `;
 
